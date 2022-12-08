1cd6392bac0b9e15a7d0504e7c3c354c3a7c2e66
unknown <mbaremedsalemmbare@gmail.com>
2022-12-08 17:16:51 +0100

    deplay on python any whrere

--- Inscrire/admin.py
+++ Inscrire/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from Inscrire.models import Etudiant
 # Register your models here.
+
+admin.site.register(Etudiant)
--- Inscrire/models.py
+++ Inscrire/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Etudiant(models.Model):
+     nom= models.CharField(max_length=200)
+     photoEtudiant=models.ImageField(null=True)
+     nni=models.IntegerField()
+     sexe=models.CharField(max_length=200)
+     dateNaissance=models.DateField()
+     lieuNaissance= models.CharField(max_length=200)
+     telephone = models.CharField(max_length=200)
+     email = models.CharField(max_length=200)
+     carteIdentite=models.ImageField(null=True)
+     numBac=models.IntegerField()
+     serie= models.CharField(max_length=200)
+     releveNote=models.ImageField(null=True)
+     session= models.CharField(max_length=200)
+     moyenGeneral=models.FloatField()
+     pays= models.CharField(max_length=200)
+     AnObtantionBac=models.DateField()
+     niveau=models.CharField(max_length=200, null=True)
+
+     def __str__(self):  
+        return self.nom
